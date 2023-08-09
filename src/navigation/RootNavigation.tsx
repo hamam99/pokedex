400f828e52bdbebf4506f9594c4d02e738dca538
Hamam <zero.saturnus@gmail.com>
2023-08-09 21:53:09 +0700

    add header in navigation

--- src/navigation/RootNavigation.tsx
+++ src/navigation/RootNavigation.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import PokemonList from '../pages/PokemonList';
 import PokemonDetail from '../pages/PokemonDetail';
+import Header from '../components/Header';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,12 +12,22 @@ const RootNavigation = () => {
       <Stack.Screen
         name="PokemonList"
         component={PokemonList}
-        options={{headerShown: false}}
+        options={{
+          headerShown: true,
+          header: () => {
+            return <Header title="Pokemon List" />;
+          },
+        }}
       />
       <Stack.Screen
         name="PokemonDetail"
         component={PokemonDetail}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          header: () => {
+            return <Header title="Pokemon Detail" withBack={true} />;
+          },
+        }}
       />
     </Stack.Navigator>
   );
