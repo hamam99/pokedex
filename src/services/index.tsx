7e60eb0169d684d6059ed7bc14a46b16df8d6468
Hamam <zero.saturnus@gmail.com>
2023-08-09 23:21:31 +0700

    add service to get pokemon

---
+++ src/services/index.tsx
@@ -0,0 +1,35 @@
+import {useRecoilState} from 'recoil';
+import AxiosClient from './AxiosClient';
+import ListUrl from './ListUrl';
+import {
+  IRequest,
+  IResponseFormat,
+  ResponseListPokemon,
+  ResultListPokemon,
+} from './types';
+
+export const getListPokemon = async ({
+  limit = 8,
+  offset = 0,
+}: IRequest): Promise<IResponseFormat<ResultListPokemon[], String>> => {
+  try {
+    const res = await AxiosClient.get(ListUrl.listPokemon, {
+      params: {
+        limit,
+        offset,
+      },
+    });
+    const data = res.data as ResponseListPokemon;
+
+    return {
+      response: data.results,
+      error: null,
+    };
+  } catch (err) {
+    const errorData = err?.response?.message;
+    return {
+      response: [],
+      error: errorData ?? 'Terjadi kesalahan, coba lagi!',
+    };
+  }
+};
