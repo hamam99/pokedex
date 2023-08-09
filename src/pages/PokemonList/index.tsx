14c75ab0ce46923a6878cc4f1ce504b9685731bc
Hamam <zero.saturnus@gmail.com>
2023-08-09 20:33:40 +0700

    crate screenn pokemon list

---
+++ src/pages/PokemonList/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const PokemonList = () => {
+  return (
+    <View>
+      <Text>PokemonList</Text>
+    </View>
+  );
+};
+
+export default PokemonList;
