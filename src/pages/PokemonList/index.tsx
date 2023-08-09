110bd6aef2093757193a29ad2b271214018e66ee
Hamam <zero.saturnus@gmail.com>
2023-08-09 23:31:59 +0700

    removing mock

--- src/pages/PokemonList/index.tsx
+++ src/pages/PokemonList/index.tsx
@@ -8,48 +8,6 @@ import {useRecoilState, useRecoilValue} from 'recoil';
 import {getListPokemon} from '../../services';
 
 const PokemonList = () => {
-  const MockData = [
-    {
-      name: 'bulbasaur',
-      url: 'https://pokeapi.co/api/v2/pokemon/1/',
-    },
-    {
-      name: 'ivysaur',
-      url: 'https://pokeapi.co/api/v2/pokemon/2/',
-    },
-    {
-      name: 'venusaur',
-      url: 'https://pokeapi.co/api/v2/pokemon/3/',
-    },
-    {
-      name: 'charmander',
-      url: 'https://pokeapi.co/api/v2/pokemon/4/',
-    },
-    {
-      name: 'charmeleon',
-      url: 'https://pokeapi.co/api/v2/pokemon/5/',
-    },
-    {
-      name: 'charizard',
-      url: 'https://pokeapi.co/api/v2/pokemon/6/',
-    },
-    {
-      name: 'squirtle',
-      url: 'https://pokeapi.co/api/v2/pokemon/7/',
-    },
-    {
-      name: 'wartortle',
-      url: 'https://pokeapi.co/api/v2/pokemon/8/',
-    },
-    {
-      name: 'blastoise',
-      url: 'https://pokeapi.co/api/v2/pokemon/9/',
-    },
-    {
-      name: 'caterpie',
-      url: 'https://pokeapi.co/api/v2/pokemon/10/',
-    },
-  ];
   const {width} = useWindowDimensions();
   const [loading, setLoading] = useState(false);
   const [loadMore, setLoadMore] = useState(false);
