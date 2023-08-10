c61f57b23d2bcc9909d9b3301bb417813c311d8b
Hamam <zero.saturnus@gmail.com>
2023-08-11 00:24:45 +0700

    add function to drill get pokemon evolition function

---
+++ src/utils/getPokemonEvolutionChain.tsx
@@ -0,0 +1,23 @@
+import {
+  Chain,
+  EvolutionChainTypes,
+} from '../services/types/EvolutionChainTypes';
+
+function getPokemonEvolutionChain(
+  evolutionChainData: EvolutionChainTypes,
+): string[] {
+  const pokemonNames: string[] = [];
+
+  function extractEvolutionName(chain: Chain) {
+    pokemonNames.push(chain.species.name);
+    if (chain.evolves_to.length > 0) {
+      chain.evolves_to.forEach(subChain => extractEvolutionName(subChain));
+    }
+  }
+
+  extractEvolutionName(evolutionChainData.chain);
+
+  return pokemonNames;
+}
+
+export default getPokemonEvolutionChain;
