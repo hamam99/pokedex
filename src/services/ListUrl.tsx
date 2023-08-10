98aaaba9870d7055f8dd50e24945687676f5bbb2
Hamam <zero.saturnus@gmail.com>
2023-08-11 00:26:03 +0700

    add url api

--- src/services/ListUrl.tsx
+++ src/services/ListUrl.tsx
@@ -1,5 +1,8 @@
 const ListUrl = {
   listPokemon: 'pokemon',
+  detailPokemon: (name: string) => `pokemon/${name}`,
+  pokemonSpecies: (id: string) => `pokemon-species/${id}`,
+  evolutionChain: (id: string) => `evolution-chain/${id}`,
 };
 
 export default ListUrl;
