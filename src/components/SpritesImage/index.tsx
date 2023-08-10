17c245df08da8c70984489ce397ca0b12bf4296d
Hamam <zero.saturnus@gmail.com>
2023-08-10 23:13:55 +0700

    add component sprites image

---
+++ src/components/SpritesImage/index.tsx
@@ -0,0 +1,33 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import FastImage from 'react-native-fast-image';
+
+const SpritesImage = ({url, label}: {url: string; label: string}) => {
+  return (
+    <View
+      style={{
+        width: 80,
+        alignItems: 'center',
+        borderWidth: 1,
+        borderRadius: 4,
+        borderColor: '#6a6a6a',
+      }}>
+      <FastImage
+        style={{width: 70, height: 70}}
+        source={{
+          uri: url,
+          priority: FastImage.priority.normal,
+        }}
+        resizeMode={FastImage.resizeMode.contain}
+      />
+      <Text
+        style={{
+          paddingBottom: 2,
+        }}>
+        {label}
+      </Text>
+    </View>
+  );
+};
+
+export default SpritesImage;
