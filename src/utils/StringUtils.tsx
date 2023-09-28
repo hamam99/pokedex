2050515c6153375be7212db70f7f7940cf4425fa
Hamam <hamam.nasrodin@maybank.com>
2023-09-28 17:27:40 +0700

    add function to get offset

--- src/utils/StringUtils.tsx
+++ src/utils/StringUtils.tsx
@@ -4,6 +4,28 @@ function getIdFromUrl(url: string): string {
   return id;
 }
 
-const StringUtils = {getIdFromUrl};
+function getOffsetFromUrl(urlString: string) {
+  const parts = urlString.split('?');
+  const queryParams = parts[1].split('&');
+  let offset = '',
+    limit = '';
+
+  for (let i = 0; i < queryParams.length; i++) {
+    let param = queryParams[i].split('=');
+
+    if (param[0] === 'offset') {
+      offset = param[1];
+    } else if (param[0] === 'limit') {
+      limit = param[1];
+    }
+  }
+
+  return {
+    offset,
+    limit,
+  };
+}
+
+const StringUtils = {getIdFromUrl, getOffsetFromUrl};
 
 export default StringUtils;
