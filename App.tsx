41b7cc6800d7812e16bffc275f07066fc17c0070
Hamam <hamam.nasrodin@maybank.com>
2023-09-28 17:23:56 +0700

    add react query

--- App.tsx
+++ App.tsx
@@ -10,18 +10,22 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {RecoilRoot} from 'recoil';
 import AppNavigation from './src/navigation/AppNavigation';
 import {PaperProvider} from 'react-native-paper';
+import {QueryClient, QueryClientProvider} from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
-    <RecoilRoot>
-      <PaperProvider>
-        <SafeAreaView style={{flex: 1}}>
-          <AppNavigation />
-        </SafeAreaView>
-      </PaperProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <PaperProvider>
+          <SafeAreaView style={{flex: 1}}>
+            <AppNavigation />
+          </SafeAreaView>
+        </PaperProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
-
 
 export default App;
