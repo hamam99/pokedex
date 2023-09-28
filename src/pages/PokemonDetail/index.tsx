d48801c54a4452dae3d01cc86847bc6b1fb616a0
Hamam <hamam.nasrodin@maybank.com>
2023-09-28 22:08:14 +0700

    refactoring

--- src/pages/PokemonDetail/index.tsx
+++ src/pages/PokemonDetail/index.tsx
@@ -6,21 +6,12 @@ import HeightAndWeight from './HeightAndWeight';
 import Types from './Types';
 import Moves from './Moves';
 import Name from './Name';
-import {ActivityIndicator, MD2Colors} from 'react-native-paper';
 import EvolutionChain from './EvolutionChain';
-import useGetDetailPokemon from '../../hooks/useGetDetailPokemon';
 
 const PokemonDetail = () => {
   const route = useRoute();
   const namePokemon = route.params?.name;
   const idPokemon = route.params?.id;
-
-  const {loading} = useGetDetailPokemon(namePokemon);
-
-  if (loading) {
-    return <ActivityIndicator animating={loading} color={MD2Colors.blue500} />;
-  }
-
   return (
     <View style={{paddingTop: 8, paddingHorizontal: 12, flex: 1, rowGap: 8}}>
       <Name name={namePokemon} />
